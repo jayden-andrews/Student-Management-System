a9dc1f72bd4912a77a94a8acfd51ae7dcee4f2c8
Jayden Andrews <jaydenrandrews@gmail.com>
2026-02-25 14:40:21 -0500

    Implemented Student class

--- src/main/java/students/jaydenandrews/Person.java
+++ src/main/java/students/jaydenandrews/Person.java
@@ -1,7 +1,27 @@
 package students.jaydenandrews;
 
-public class Person {
+public abstract class Person {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
 
+    public Person(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public abstract void getSummary();
     public static void main(String[] args) {
 
     }
---
+++ src/main/java/students/jaydenandrews/Student.java
@@ -0,0 +1,22 @@
+package students.jaydenandrews;
+
+public class Student extends Person {
+    private int gradeLevel;
+    private double gpa;
+
+    public Student(String id, String firstName, String lastName, String email, int gradeLevel, double gpa) {
+        super(id, firstName,lastName, email);
+        this.gradeLevel = gradeLevel;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public void getSummary() {
+        System.out.println("[Student] " + this.getFirstName() + " " + this.getLastName() + " | " +
+                "Grade Level: " + this.gradeLevel + " | GPA: " + this.gpa + " | Honor Roll: " + isOnHonorRoll());
+    }
+
+    public boolean isOnHonorRoll() {
+        return (this.gpa > 3.0);
+    }
+}
